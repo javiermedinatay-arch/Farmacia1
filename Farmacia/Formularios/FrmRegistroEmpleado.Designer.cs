1b54a11c2dd135779aabcc53cff80a76667ace5f
javiermedinatay-arch <javiermedinatay@gmail.com>
2026-05-05 07:34:30 -0500

    Se agrego Registro Empleado

---
+++ Farmacia/Formularios/FrmRegistroEmpleado.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Farmacia.Formularios
+{
+    partial class FrmRegistroEmpleado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.Label();
+            this.NOMBRE = new System.Windows.Forms.Label();
+            this.CARGO = new System.Windows.Forms.Label();
+            this.TELEFONO = new System.Windows.Forms.Label();
+            this.CORREO = new System.Windows.Forms.Label();
+            this.SUELDO = new System.Windows.Forms.Label();
+            this.FECHACONTRATO = new System.Windows.Forms.Label();
+            this.TxtId = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtCargo = new System.Windows.Forms.TextBox();
+            this.TxtTel = new System.Windows.Forms.TextBox();
+            this.TxtCorreo = new System.Windows.Forms.TextBox();
+            this.TxtSueldo = new System.Windows.Forms.TextBox();
+            this.TxtFechaContrato = new System.Windows.Forms.TextBox();
+            this.BTNNUEVO = new System.Windows.Forms.Button();
+            this.BTNGUARDAR = new System.Windows.Forms.Button();
+            this.BTNEDITAR = new System.Windows.Forms.Button();
+            this.BTNELIMINAR = new System.Windows.Forms.Button();
+            this.BTNMOSTRAR = new System.Windows.Forms.Button();
+            this.dgvEmpleado = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(396, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRO EMPLEADO";
+            // 
+            // id
+            // 
+            this.id.AutoSize = true;
+            this.id.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.id.Location = new System.Drawing.Point(107, 72);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(20, 15);
+            this.id.TabIndex = 1;
+            this.id.Text = "ID";
+            // 
+            // NOMBRE
+            // 
+            this.NOMBRE.AutoSize = true;
+            this.NOMBRE.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NOMBRE.Location = new System.Drawing.Point(107, 99);
+            this.NOMBRE.Name = "NOMBRE";
+            this.NOMBRE.Size = new System.Drawing.Size(49, 15);
+            this.NOMBRE.TabIndex = 2;
+            this.NOMBRE.Text = "Nombre";
+            // 
+            // CARGO
+            // 
+            this.CARGO.AutoSize = true;
+            this.CARGO.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CARGO.Location = new System.Drawing.Point(107, 129);
+            this.CARGO.Name = "CARGO";
+            this.CARGO.Size = new System.Drawing.Size(39, 15);
+            this.CARGO.TabIndex = 3;
+            this.CARGO.Text = "Cargo";
+            // 
+            // TELEFONO
+            // 
+            this.TELEFONO.AutoSize = true;
+            this.TELEFONO.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TELEFONO.Location = new System.Drawing.Point(107, 165);
+            this.TELEFONO.Name = "TELEFONO";
+            this.TELEFONO.Size = new System.Drawing.Size(53, 15);
+            this.TELEFONO.TabIndex = 4;
+            this.TELEFONO.Text = "Telefono";
+            // 
+            // CORREO
+            // 
+            this.CORREO.AutoSize = true;
+            this.CORREO.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CORREO.Location = new System.Drawing.Point(107, 194);
+            this.CORREO.Name = "CORREO";
+            this.CORREO.Size = new System.Drawing.Size(44, 15);
+            this.CORREO.TabIndex = 5;
+            this.CORREO.Text = "Correo";
+            // 
+            // SUELDO
+            // 
+            this.SUELDO.AutoSize = true;
+            this.SUELDO.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SUELDO.Location = new System.Drawing.Point(107, 228);
+            this.SUELDO.Name = "SUELDO";
+            this.SUELDO.Size = new System.Drawing.Size(44, 15);
+            this.SUELDO.TabIndex = 6;
+            this.SUELDO.Text = "Sueldo";
+            // 
+            // FECHACONTRATO
+            // 
+            this.FECHACONTRATO.AutoSize = true;
+            this.FECHACONTRATO.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FECHACONTRATO.Location = new System.Drawing.Point(107, 267);
+            this.FECHACONTRATO.Name = "FECHACONTRATO";
+            this.FECHACONTRATO.Size = new System.Drawing.Size(84, 15);
+            this.FECHACONTRATO.TabIndex = 7;
+            this.FECHACONTRATO.Text = "Fecha contrato";
+            // 
+            // TxtId
+            // 
+            this.TxtId.Location = new System.Drawing.Point(273, 72);
+            this.TxtId.Name = "TxtId";
+            this.TxtId.Size = new System.Drawing.Size(330, 20);
+            this.TxtId.TabIndex = 8;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(273, 99);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(330, 20);
+            this.TxtNombre.TabIndex = 9;
+            // 
+            // TxtCargo
+            // 
+            this.TxtCargo.Location = new System.Drawing.Point(273, 129);
+            this.TxtCargo.Name = "TxtCargo";
+            this.TxtCargo.Size = new System.Drawing.Size(330, 20);
+            this.TxtCargo.TabIndex = 10;
+            // 
+            // TxtTel
+            // 
+            this.TxtTel.Location = new System.Drawing.Point(273, 160);
+            this.TxtTel.Name = "TxtTel";
+            this.TxtTel.Size = new System.Drawing.Size(330, 20);
+            this.TxtTel.TabIndex = 11;
+            // 
+            // TxtCorreo
+            // 
+            this.TxtCorreo.Location = new System.Drawing.Point(273, 194);
+            this.TxtCorreo.Name = "TxtCorreo";
+            this.TxtCorreo.Size = new System.Drawing.Size(330, 20);
+            this.TxtCorreo.TabIndex = 12;
+            // 
+            // TxtSueldo
+            // 
+            this.TxtSueldo.Location = new System.Drawing.Point(273, 228);
+            this.TxtSueldo.Name = "TxtSueldo";
+            this.TxtSueldo.Size = new System.Drawing.Size(330, 20);
+            this.TxtSueldo.TabIndex = 13;
+            // 
+            // TxtFechaContrato
+            // 
+            this.TxtFechaContrato.Location = new System.Drawing.Point(273, 267);
+            this.TxtFechaContrato.Name = "TxtFechaContrato";
+            this.TxtFechaContrato.Size = new System.Drawing.Size(330, 20);
+            this.TxtFechaContrato.TabIndex = 14;
+            // 
+            // BTNNUEVO
+            // 
+            this.BTNNUEVO.Location = new System.Drawing.Point(69, 311);
+            this.BTNNUEVO.Name = "BTNNUEVO";
+            this.BTNNUEVO.Size = new System.Drawing.Size(122, 48);
+            this.BTNNUEVO.TabIndex = 15;
+            this.BTNNUEVO.Text = "NUEVO";
+            this.BTNNUEVO.UseVisualStyleBackColor = true;
+            this.BTNNUEVO.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BTNGUARDAR
+            // 
+            this.BTNGUARDAR.Location = new System.Drawing.Point(242, 311);
+            this.BTNGUARDAR.Name = "BTNGUARDAR";
+            this.BTNGUARDAR.Size = new System.Drawing.Size(122, 48);
+            this.BTNGUARDAR.TabIndex = 16;
+            this.BTNGUARDAR.Text = "GUARDAR";
+            this.BTNGUARDAR.UseVisualStyleBackColor = true;
+            this.BTNGUARDAR.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BTNEDITAR
+            // 
+            this.BTNEDITAR.Location = new System.Drawing.Point(437, 311);
+            this.BTNEDITAR.Name = "BTNEDITAR";
+            this.BTNEDITAR.Size = new System.Drawing.Size(122, 48);
+            this.BTNEDITAR.TabIndex = 17;
+            this.BTNEDITAR.Text = "EDITAR";
+            this.BTNEDITAR.UseVisualStyleBackColor = true;
+            // 
+            // BTNELIMINAR
+            // 
+            this.BTNELIMINAR.Location = new System.Drawing.Point(621, 311);
+            this.BTNELIMINAR.Name = "BTNELIMINAR";
+            this.BTNELIMINAR.Size = new System.Drawing.Size(122, 48);
+            this.BTNELIMINAR.TabIndex = 18;
+            this.BTNELIMINAR.Text = "ELIMINAR";
+            this.BTNELIMINAR.UseVisualStyleBackColor = true;
+            // 
+            // BTNMOSTRAR
+            // 
+            this.BTNMOSTRAR.Location = new System.Drawing.Point(653, 84);
+            this.BTNMOSTRAR.Name = "BTNMOSTRAR";
+            this.BTNMOSTRAR.Size = new System.Drawing.Size(122, 48);
+            this.BTNMOSTRAR.TabIndex = 19;
+            this.BTNMOSTRAR.Text = "MOSTRAR EMPLEADO";
+            this.BTNMOSTRAR.UseVisualStyleBackColor = true;
+            // 
+            // dgvEmpleado
+            // 
+            this.dgvEmpleado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmpleado.Location = new System.Drawing.Point(29, 365);
+            this.dgvEmpleado.Name = "dgvEmpleado";
+            this.dgvEmpleado.Size = new System.Drawing.Size(776, 239);
+            this.dgvEmpleado.TabIndex = 20;
+            // 
+            // FrmRegistroEmpleado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 616);
+            this.Controls.Add(this.dgvEmpleado);
+            this.Controls.Add(this.BTNMOSTRAR);
+            this.Controls.Add(this.BTNELIMINAR);
+            this.Controls.Add(this.BTNEDITAR);
+            this.Controls.Add(this.BTNGUARDAR);
+            this.Controls.Add(this.BTNNUEVO);
+            this.Controls.Add(this.TxtFechaContrato);
+            this.Controls.Add(this.TxtSueldo);
+            this.Controls.Add(this.TxtCorreo);
+            this.Controls.Add(this.TxtTel);
+            this.Controls.Add(this.TxtCargo);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.TxtId);
+            this.Controls.Add(this.FECHACONTRATO);
+            this.Controls.Add(this.SUELDO);
+            this.Controls.Add(this.CORREO);
+            this.Controls.Add(this.TELEFONO);
+            this.Controls.Add(this.CARGO);
+            this.Controls.Add(this.NOMBRE);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmRegistroEmpleado";
+            this.Text = "FrmRegistroEmpleado";
+            this.Load += new System.EventHandler(this.FrmRegistroEmpleado_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label id;
+        private System.Windows.Forms.Label NOMBRE;
+        private System.Windows.Forms.Label CARGO;
+        private System.Windows.Forms.Label TELEFONO;
+        private System.Windows.Forms.Label CORREO;
+        private System.Windows.Forms.Label SUELDO;
+        private System.Windows.Forms.Label FECHACONTRATO;
+        private System.Windows.Forms.TextBox TxtId;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.TextBox TxtCargo;
+        private System.Windows.Forms.TextBox TxtTel;
+        private System.Windows.Forms.TextBox TxtCorreo;
+        private System.Windows.Forms.TextBox TxtSueldo;
+        private System.Windows.Forms.TextBox TxtFechaContrato;
+        private System.Windows.Forms.Button BTNNUEVO;
+        private System.Windows.Forms.Button BTNGUARDAR;
+        private System.Windows.Forms.Button BTNEDITAR;
+        private System.Windows.Forms.Button BTNELIMINAR;
+        private System.Windows.Forms.Button BTNMOSTRAR;
+        private System.Windows.Forms.DataGridView dgvEmpleado;
+    }
+}
