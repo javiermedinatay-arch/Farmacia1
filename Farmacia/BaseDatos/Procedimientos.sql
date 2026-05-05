50690167b3c7e7fa1a4ed62c012820e5b41b3af2
07CHMK1PA111A07 <07CHMK1PA111A07@07CHMK1PA111A07>
2026-05-05 11:58:12 -0500

    cambiosss

--- Farmacia/BaseDatos/Procedimientos.sql
+++ Farmacia/BaseDatos/Procedimientos.sql
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿CREATE PROCEDURE farm.usp_RegistrarVentaCompleta
+    @idcliente INT,
+    @idempleado INT,
+    @tipo_comprobante NVARCHAR(50),
+    @total_venta DECIMAL(10,2),
+    @metodo_pago NVARCHAR(50),
+    @detalles farm.DetalleVentaType READONLY -- Recibe la lista del DataGridView
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        BEGIN TRANSACTION;
 
-namespace Farmacia.BaseDatos
-{
-    class Procedimientos
-    {
-    }
-}
+        -- 1. Insertar la Venta principal
+        DECLARE @idventa_generada INT;
+        
+        INSERT INTO farm.venta (idcliente, idempleado, tipo_comprobante, total_venta, estado_pago)
+        VALUES (@idcliente, @idempleado, @tipo_comprobante, @total_venta, 'Pagado');
+
+        SET @idventa_generada = SCOPE_IDENTITY();
+
+        -- 2. Insertar el Pago relacionado
+        INSERT INTO farm.pago (idventa, metodo_pago, monto_pagado)
+        VALUES (@idventa_generada, @metodo_pago, @total_venta);
+
+        -- 3. Insertar Detalles y Actualizar Stock
+        INSERT INTO farm.detalle_venta (idventa, idproducto, cantidad, precio_unitario)
+        SELECT @idventa_generada, idproducto, cantidad, precio_unitario
+        FROM @detalles;
+
+        -- 4. Actualizar Stock en la tabla Producto
+        UPDATE p
+        SET p.stock_actual = p.stock_actual - d.cantidad
+        FROM farm.producto p
+        INNER JOIN @detalles d ON p.idproducto = d.idproducto;
+
+        COMMIT TRANSACTION;
+        PRINT 'Venta procesada exitosamente.';
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+        
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR (@ErrorMessage, 16, 1);
+    END CATCH
+END;
+GO
+
+CREATE TYPE farm.DetalleVentaType AS TABLE (
+    idproducto INT,
+    cantidad INT,
+    precio_unitario DECIMAL(10,2)
+);
+GO
--- Farmacia/BaseDatos/Vistas.sql
+++ Farmacia/BaseDatos/Vistas.sql
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿-- Vista para el ComboBox de Empleados
+CREATE VIEW farm.vw_ListarEmpleados AS
+SELECT idempleado, (nombres + ' ' + apellidos) AS nombre_completo, idcargo
+FROM farm.empleado;
+GO
 
-namespace Farmacia.BaseDatos
-{
-    class Vistas
-    {
-    }
-}
+-- Vista para el ComboBox de Clientes
+CREATE VIEW farm.vw_ListarClientes AS
+SELECT idcliente, (nombres + ' ' + apellidos) AS nombre_completo, dni
+FROM farm.cliente;
+GO
+
+-- Vista para el ComboBox de Productos y validación de Stock
+CREATE VIEW farm.vw_ListarProductos AS
+SELECT p.idproducto, p.nombre_producto, p.precio_venta, p.stock_actual, 
+       m.nombre_marca, c.nombre_categoria
+FROM farm.producto p
+INNER JOIN farm.marca m ON p.idmarca = m.idmarca
+INNER JOIN farm.categoria c ON p.idcategoria = c.idcategoria
+WHERE p.estado = 1;
+GO
+
+CREATE VIEW farm.vw_ResumenVentas AS
+SELECT 
+    v.idventa,
+    v.fecha_venta,
+    (c.nombres + ' ' + c.apellidos) AS cliente,
+    (e.nombres + ' ' + e.apellidos) AS vendedor,
+    v.tipo_comprobante,
+    v.total_venta,
+    p.metodo_pago
+FROM farm.venta v
+INNER JOIN farm.cliente c ON v.idcliente = c.idcliente
+INNER JOIN farm.empleado e ON v.idempleado = e.idempleado
+INNER JOIN farm.pago p ON v.idventa = p.idventa;
+GO
