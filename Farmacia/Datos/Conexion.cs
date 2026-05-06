f27a440e617a16b34346ba9d1098c66b698702f7
07CHMK1PA111A07 <07CHMK1PA111A07@07CHMK1PA111A07>
2026-05-06 07:48:58 -0500

    si

--- Farmacia/Datos/Conexion.cs
+++ Farmacia/Datos/Conexion.cs
@@ -9,12 +9,12 @@ namespace Farmacia.Datos
 {
     public class Conexion
     {
-        private string cadena = @"Server=(localdb)\MSSQLLocalDB;Database=FarmaciaDB;Trusted_Connection=True;";}
+        private string cadena = @"Server=(localdb)\MSSQLLocalDB;Database=FarmaciaDB;Trusted_Connection=True;";
         //private string cadena = @"Server=E74976425;Database=FarmaciaDB;Trusted_Connection=True;";
 
         public SqlConnection GetConexion()
         {
             return new SqlConnection(cadena);
         }
-}
+    }
 }
